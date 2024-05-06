33e3f88d7b188993cd66c7228f117822801d3a9a
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-05-06 22:08:49 +0200

    .

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(
   cors({
-    origin: ['http://localhost:3002', 'reactbird.com', 'http://52.78.142.0'],
+    origin: ['http://localhost:3002', 'nodebird.com', 'http://52.78.142.0'],
     credentials: true,
   }),
 );
