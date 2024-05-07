e10dd461e4275ea7eba965f6cc9c0dd6af4bd76b
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-05-07 16:00:09 +0200

    update lambda index

--- apps/lambda/index.js
+++ apps/lambda/index.js
@@ -1,7 +1,6 @@
 const { AWS } = require('aws-sdk');
 const sharp = require('sharp');
 
-// const s3 = new AWS.S3();
 const { S3Client,
   GetObjectCommand,
   PutObjectCommand
@@ -21,7 +20,6 @@ exports.handler = async (event, context, callback) => {
   try {
     const getObjectParams = {Bucket, Key};
     const s3Object = await s3Client.send(new GetObjectCommand(getObjectParams));
-    // const s3Object = await s3.getObject({Bucket, Key}).promise();
     console.log('original', s3Object.Body.length);
     const resizedImage = await sharp(s3Object.Body)
       .resize(400,400, { fit: 'inside'})
@@ -35,11 +33,6 @@ exports.handler = async (event, context, callback) => {
 
     };
     await s3Client.send(new PutObjectCommand(putObjectParams));
-    // await s3.putObject({
-    //   Bucket,
-    //   Key: `thumb/${filename}`,
-    //   Body: resizedImage,
-    // }).promise();
     console.log('put', resizedImage.length);
     return callback(null, `thumb/${filename}`)
 
