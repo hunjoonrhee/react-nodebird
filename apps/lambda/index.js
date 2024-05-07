ae7777c489ac9ac47a2b5a1d345c854762548a62
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-05-07 15:39:21 +0200

    update lambda index

--- apps/lambda/index.js
+++ apps/lambda/index.js
@@ -1,7 +1,13 @@
 const { AWS } = require('aws-sdk');
 const sharp = require('sharp');
 
-const s3 = new AWS.S3();
+// const s3 = new AWS.S3();
+const { S3Client,
+  GetObjectCommand,
+  PutObjectCommand
+} = require('@aws-sdk/client-s3');
+const s3Client = new S3Client({region: 'ap-northeast-2'});
+
 
 exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
@@ -13,18 +19,27 @@ exports.handler = async (event, context, callback) => {
   console.log('filename', filename, 'ext', ext)
 
   try {
-    const s3Object = await s3.getObject({Bucket, Key}).promise();
+    const getObjectParams = {Bucket, Key};
+    const s3Object = await s3Client.send(new GetObjectCommand(getObjectParams));
+    // const s3Object = await s3.getObject({Bucket, Key}).promise();
     console.log('original', s3Object.Body.length);
     const resizedImage = await sharp(s3Object.Body)
       .resize(400,400, { fit: 'inside'})
       .toFormat(requiredFormat)
       .toBuffer();
 
-    await s3.putObject({
+    const putObjectParams = {
       Bucket,
       Key: `thumb/${filename}`,
       Body: resizedImage,
-    }).promise();
+
+    };
+    await s3Client.send(new PutObjectCommand(putObjectParams));
+    // await s3.putObject({
+    //   Bucket,
+    //   Key: `thumb/${filename}`,
+    //   Body: resizedImage,
+    // }).promise();
     console.log('put', resizedImage.length);
     return callback(null, `thumb/${filename}`)
 
